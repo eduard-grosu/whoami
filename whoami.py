9e4694c669e9bcc3065cfc4b3d3eb3c6ef14ef43
Eduard Grosu <work@eduardgro.su>
2023-09-04 02:26:21 +0300

    rewrite

--- whoami.py
+++ whoami.py
@@ -2,7 +2,9 @@ from dateutil.relativedelta import relativedelta
 from flask import Flask
 import datetime
 
+
 app = Flask(__name__)
+app.config['JSON_AS_ASCII'] = False
 app.config['JSON_SORT_KEYS'] = False
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
 
@@ -11,7 +13,7 @@ age_time = datetime.datetime(2000, 3, 23)
 work_time = datetime.datetime(2021, 10, 1)
 linux_time = datetime.datetime(2014, 1, 1) # somewhere in 2014
 python_time = datetime.datetime(2017, 3, 1) # somewhere in march 2017
-university_time = datetime.datetime(2020, 10, 1)
+
 
 def beautify_time(date1, date2, *, approx=None):
     builder = []
@@ -28,12 +30,11 @@ def beautify_time(date1, date2, *, approx=None):
 
     return ', '.join(builder) + ' ago'
 
+
 @app.route('/whoami')
 def root():
     now = datetime.datetime.utcnow()
-
     age = relativedelta(now, age_time)
-    university = relativedelta(now, university_time)
 
     return {
         'whoami': {
@@ -41,11 +42,20 @@ def root():
             'age': age.years,
             'country': 'Romania'
         },
-        'university': {
-            'at': 'Spiru Haret University',
-            'as': 'Computer Science',
-            'year': university.years + 1 # count the start of the first year
-        },
+        'education': [
+            {
+                'name': 'Victor Babeş National College',
+                'field': 'Natural Sciences',
+                'when': '2015-2019',
+                'degree': 'Baccalaureate Degree'
+            },
+            {
+                'name': 'Spiru Haret University',
+                'field': 'Computer Science',
+                'when': '2020-2023',
+                'degree': 'Bachelor\'s Degree'
+            }
+        ],
         'hobbies': [
             {
                 'hobby': 'Python Developer',
@@ -57,11 +67,12 @@ def root():
             }
         ],
         'work': {
-            'as': 'Backend Software Engineer',
-            'for': 'Metro Systems Romania',
+            'as': 'DevOps Engineer',
+            'for': 'Wipro Technology Solutions SRL',
             'since': f'{work_time.strftime("%B %Y")} ({beautify_time(now, work_time)})'
         }
     }
+
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
